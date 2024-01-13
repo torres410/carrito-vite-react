67682d9bd7da6ebced028746374c8b4aa01ede80
Ricardo Torres <giron.torres1@gmail.com>
2024-01-12 23:03:21 -0600

    Productos iniciales

---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://images.unsplash.com/photo-1604671801908-6f0c6a092c05?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		nombreProducto: 'Zapatos',
+		precio: 90,
+		cantidad: 1,
+	},
+	{
+		id: 2,
+		img: 'https://images.unsplash.com/photo-1552857131-0afaed1953f5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		nombreProducto: 'Laptop',
+		precio: 800,
+		cantidad: 1,
+	},
+	{
+		id: 3,
+		img: 'https://images.unsplash.com/photo-1623820919239-0d0ff10797a1?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+		nombreProducto: 'Mouse',
+		precio: 35,
+		cantidad: 1,
+	},
+];
