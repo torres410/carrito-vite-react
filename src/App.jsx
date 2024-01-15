0966af5fd18449c17cea78e077ef60443b84e9ef
Ricardo Torres <giron.torres1@gmail.com>
2024-01-15 01:10:10 -0600

    Constantes con useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import { useState } from 'react';
 
 function App() {
+	const [totalProductos, setTotalProductos] = useState([]);
+	const [total, setTotal] = useState(0);
+	const [conteoProductos, setConteoProductos] = useState(0);
 
 return (
 	
